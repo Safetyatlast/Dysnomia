29a91112ad32bdab786313ad321e540099bbbdf5
Moto <motoenduroboy@gmail.com>
2020-01-23 11:35:55 +1100

    Add Handler to index.js and make Dysnomia a function that returns a new handler

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 "use strict";
 
-const Dysnomia = {};
+const Handler = require('./lib/structures/Handler');
 
+function Dysnomia (options) {
+  return new Handler(options);
+}
+
+Dysnomia.Handler = Handler;
 Dysnomia.Command = require('./lib/structures/Command');
 
 module.exports = Dysnomia;
