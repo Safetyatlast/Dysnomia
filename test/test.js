e21d6f055c3a4db22b8f3cf1649985cdf8738cc7
Moto <motoenduroboy@gmail.com>
2020-01-23 15:25:29 +1100

    Add Cooldown

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
+const Handler = new (require('../lib/structures/Handler')) ({ prefix: '&' });
 
 describe('Handler', function() {
-  const Handler = new (require('../lib/structures/Handler')) ({ prefix: '&' });
   describe('#setGuildPrefix()', function() {
     Handler.setGuildPrefix('401134835818692608', '!');
     it('should add the prefix as a value and guildID as key to the guildPrefixes map', function() {
@@ -63,6 +63,26 @@ describe('Handler', function() {
         },
         content: "&ping pong !"
       }), { Command, Content: "!" });
+    });
+  });
+});
+
+describe('Command', function() {
+  const Command = new (require('../lib/structures/Command')) ({names: ["cooldown"], cooldown: 10000});
+  Handler.registerCommands(Command);
+  describe('#updateCooldown', function() {
+    it('should add user to the cooldowns map', function() {
+      Command.updateCooldown("158594933274574849");
+      assert.equal(Command.cooldowns.has("158594933274574849"), true);
+    });
+  });
+  describe('#checkCooldown', function() {
+    it('should return false when user is not on a cooldown', function() {
+      assert.equal(Command.checkCooldown("279866000533618689"), false);
+    });
+    it('should return true when user is on a cooldown', function() {
+      Command.updateCooldown("279866000533618689");
+      assert.equal(Command.checkCooldown("279866000533618689"), true);
     });
   });
 });
