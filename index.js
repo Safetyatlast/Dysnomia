4f0e895275169f7203a51f0c6670892d55f8e24d
Moto <motoenduroboy@gmail.com>
2020-01-27 22:06:48 +1100

    Added Middleware.js and Preconditions.js structures to index.js Added Constants.js to index.js

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function Dysnomia (options) {
 
 Dysnomia.Handler = Handler;
 Dysnomia.Command = require('./lib/structures/Command');
+Dysnomia.Middleware = require('./lib/structures/Middleware');
+Dysnomia.Precondition = require('./lib/structures/Precondition');
+
+Dysnomia.Constants = require('./lib/Constants');
 
 module.exports = Dysnomia;
