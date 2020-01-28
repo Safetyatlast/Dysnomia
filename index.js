298110b7bc427c649532c2878ea89b95b3a64a97
Moto <motoenduroboy@gmail.com>
2020-01-28 19:51:54 +1100

    Add Argument.js and TypeReader.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ Dysnomia.Handler = Handler;
 Dysnomia.Command = require('./lib/structures/Command');
 Dysnomia.Middleware = require('./lib/structures/Middleware');
 Dysnomia.Precondition = require('./lib/structures/Precondition');
+Dysnomia.Argument = require('./lib/structures/Argument');
+Dysnomia.TypeReader = require('./lib/structures/TypeReader');
 
 Dysnomia.Constants = require('./lib/Constants');
 
