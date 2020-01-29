9a1bb0082b0e32077d4d73b760eb1f4ca9a29bf2
Moto <motoenduroboy@gmail.com>
2020-01-29 18:05:53 +1100

    More JSDoc Stuff

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 const Handler = require('./lib/structures/Handler');
 
+/**
+ *
+ * @param {Object} options
+ * @param {String} options.prefix Default guild prefix
+ * @param {RegExp?} options.argumentRegex Regex used for spiting arguments
+ * @returns Handler
+ */
 function Dysnomia (options) {
   return new Handler(options);
 }
@@ -14,5 +21,7 @@ Dysnomia.Argument = require('./lib/structures/Argument');
 Dysnomia.TypeReader = require('./lib/structures/TypeReader');
 
 Dysnomia.Constants = require('./lib/Constants');
+
+Dysnomia.CommandError = require('./lib/enums/CommandError');
 
 module.exports = Dysnomia;
